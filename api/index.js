6cba525927357d498161428d76aa5e6013cbd896
archit1210 <architlokhande72@gmail.com>
2024-06-12 00:36:47 +0530

    Created test API route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -10,6 +11,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 });
 
 const app = express();
+
+app.use('/api/user', userRouter)
 
 app.listen(3000, () => {
     console.log("Server is listning to port 3000!!");
