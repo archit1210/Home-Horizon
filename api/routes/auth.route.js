d668b46e46bdd933c1629ee3536602d907bb73f3
archit1210 <architlokhande72@gmail.com>
2024-06-17 00:22:59 +0530

    Created signin API

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { signup } from '../controllers/auth.controller.js';
+import { signin, signup } from '../controllers/auth.controller.js';
 const router = express();
 
 router.post('/signup', signup);
+router.post('/signin', signin);
 
 export default router;
