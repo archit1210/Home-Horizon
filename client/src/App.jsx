fa8ea8106508bdf8609df086a5f320898bcd3650
archit1210 <architlokhande72@gmail.com>
2024-06-25 02:19:28 +0530

    Added Image slider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
           <Route path="/about" element={<About />}></Route>
           <Route path="/sign-in" element={<SignIn />}></Route>
           <Route path="/sign-up" element={<SignUp />}></Route>
+          <Route path="/listing/:listingId" element={<Listing />}></Route>
           <Route element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />}></Route>
             <Route path="/create-listing" element={<CreateListing />}></Route>
