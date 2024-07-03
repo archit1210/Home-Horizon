77dc56ce2b35fc5e87034ecb727b2b598a0a251e
archit1210 <architlokhande72@gmail.com>
2024-07-03 11:56:27 +0530

    Deployment

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser'
+import path from 'path'
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -14,12 +15,21 @@ mongoose.connect(process.env.MONGO).then(() => {
 });
 
 const app = express();
+
+const __dirname = path.resolve()
+
 app.use(express.json())
 app.use(cookieParser())
 
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/listing', listingRouter)
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.listen(3000, () => {
     console.log("Server is listning to port 3000!!");
